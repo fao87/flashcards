8c88ec599c171dc7d07650eb06f8872afa1368b5
fao87 <fao@meteor-it.ru>
2015-08-31 15:17:03 +0300

    add routes and change routes.rb

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,2 +1,30 @@
 class CardsController < ApplicationController
+	def index
+	end
+
+	def show
+	end
+
+	def new
+	end
+
+	def create
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	private
+
+	def find_card
+	end
+
+	def card_params
+	end
 end
