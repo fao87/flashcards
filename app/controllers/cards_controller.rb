8240f6100da248bfba30aac8552f79000c83b4a6
fao87 <fao@meteor-it.ru>
2015-09-01 23:09:00 +0300

    delete files and rewrite routes.rb index.html.erb

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,32 +1,25 @@
 class CardsController < ApplicationController
-	def index
-		@cards = Card.all
-	end
+  def index
+    @cards = Card.all
+  end
 
-	def show
-		@card = Card.find(params[:id])
-	end
+  def show
+    @card = Card.find(params[:id])
+  end
 
-	def new
-	end
+  def new
+  end
 
-	def create
-	end
+  def create
+  end
 
-	def edit
-	end
+  def edit
+  end
 
-	def update
-	end
+  def update
+  end
 
-	def destroy
-	end
-
-	private
-
-	def find_card
-	end
-
-	def card_params
-	end
+  def destroy
+  end
+  
 end
