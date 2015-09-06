3e06dbaa474ef2090eba7dee049488ad058b9502
fao87 <fao@meteor-it.ru>
2015-09-06 19:02:47 +0300

    change card.rb

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,9 +2,4 @@ class Card < ActiveRecord::Base
   validates :original_text, :translated_text, :review_date, presence: true
 
   before_create { self.review_date = review_date.next_day(3) }
-
-  #def change_date
-   # review_date.next_day(3)
-  #end
- 
 end
