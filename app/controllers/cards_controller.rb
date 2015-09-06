0c42aad3e20c0a3ae5425d5f68f0cf8c5dbf7e34
fao87 <fao@meteor-it.ru>
2015-09-06 23:21:15 +0300

    try to use simple_form

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,50 +1,49 @@
 class CardsController < ApplicationController
-	def index
-	  @cards = Card.all
-	end
+  def index
+	@cards = Card.all
+  end
 
-	def show
-	  @card = Card.find(params[:id])
-	  @card.review_date.next_day(3)
-	end
+  def show
+    @card = Card.find(params[:id])
+    @card.review_date.next_day(3)
+  end
+    
+  def new
+    @card = Card.new
+  end
 
-	def new
-	  @card = Card.new
-	end
+  def create
+    @card = Card.new(card_params)
+	@card.save
+	redirect_to @card
+  end
 
-	def create
-	  @card = Card.new(card_params)
-      @card.save
+  def edit
+    @card = Card.find(params[:id])
+  end
+
+  def update
+    @card = Card.find(params[:id])
+    if @card.update(card_params)
       redirect_to @card
-	end
+    else
+      render 'edit'
+    end
+  end
 
-	def edit
-	  @card = Card.find(params[:id])
-	end
+  def destroy
+    @card = Card.find(params[:id])
+    @card.destroy
+    redirect_to cards_path
+  end
 
-	def update
-	  @card = Card.find(params[:id])
- 
-      if @card.update(card_params)
-        redirect_to @card
-      else
-        render 'edit'
-      end
-	end
+  private
 
-	def destroy
-	  @card = Card.find(params[:id])
-      @card.destroy
-      redirect_to cards_path
-	end
+  def find_card
+  end
 
-	private
-
-	def find_card
-	end
-
-	def card_params
-		params.require(:card).permit(:original_text, :translated_text, :review_date)
-	end
+  def card_params
+    params.require(:card).permit(:original_text, :translated_text, :review_date)
+  end
 
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,5 +1,9 @@
 class Card < ActiveRecord::Base
   validates :original_text, :translated_text, :review_date, presence: true
 
-  before_create { self.review_date = review_date.next_day(3) }
+  before_create { change_date }
+
+  def change_date
+    self.review_date = review_date.next_day(3) 
+  end
 end
