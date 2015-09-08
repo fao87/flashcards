324353ab752e08ff1e43e2ebaf912a1e8d713923
fao87 <fao@meteor-it.ru>
2015-09-08 17:43:40 +0300

    add condinions for strings

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,5 +1,12 @@
 class Card < ActiveRecord::Base
   validates :original_text, :translated_text, :review_date, presence: true
+  validate :not_equal
+ 
+  def not_equal
+    if original_text.mb_chars.downcase == translated_text.mb_chars.downcase
+      errors.add(:original_text, 'совпадение недопустимо')
+    end
+  end
 
   before_create { change_date }
 
