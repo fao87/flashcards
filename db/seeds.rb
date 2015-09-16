0d4336cf62d5e86547a747afb95214a3ef9cd27c
fao87 <fao@meteor-it.ru>
2015-09-16 15:35:25 +0300

    change seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,14 +11,3 @@ page.css(".tableblue tr").each do |tr|
   Card.create(original_text: original_text, translated_text: translated_text, review_date: Time.now) 
 end
 
-
-
-
-
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
