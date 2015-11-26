b807393487debc9828b1959d8a006450bc0bbbee
fao87 <fao@meteor-it.ru>
2015-11-26 23:44:31 +0100

    create tests for scopes and empty fields

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
-require 'spec_helper'
 
 describe Card do
   it "should be invalid" do 
@@ -23,5 +22,25 @@ describe Card do
     expect(card.review_date).to eq Date.today + 3
   end
 
+  it 'should have review_date <= today when ready for review' do
+    card = Card.create(original_text: 'buy buy', translated_text: 'пока пока', review_date: Date.today)
+    expect(Card.expired.count).to eq 1
+  end
+
+  it 'check_translation words with different type оf letters' do
+    card = Card.create(original_text: 'House', translated_text: 'ДоМ', review_date: Date.today)
+    check_result = card.check_translation('HOUse')
+    expect(check_result).to be true
+  end
+
+  it 'create card with empty original text' do
+    card = Card.create(original_text: nil, translated_text: 'house', review_date: Date.today)
+    expect(card).not_to be_valid
+  end
+
+  it 'create card with empty translated text' do
+    card = Card.create(original_text: 'дом', translated_text: nil, review_date: Date.today)
+    expect(card).not_to be_valid
+  end
 end
 
